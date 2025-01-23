c1a5bbf97d2683a06ed6365f262e6bf343a3b599
Maxim <maxim.dudaryonok@yandex.ru>
2025-01-23 19:42:29 +0200

    fix: 04-copy-directory delete file after update

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,58 +1,66 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-function copyDir() {
+async function copyDir() {
   const sourceDir = path.join(__dirname, 'files');
   const destDir = path.join(__dirname, 'files-copy');
 
-  let sourceStats;
-  fs.stat(sourceDir)
-    .then(() => (sourceStats = fs.stat(destDir)))
-    .then((destStats) => {
+  try {
+    const sourceStats = await fs.stat(sourceDir);
+    let destStats;
+    try {
+      destStats = await fs.stat(destDir);
       if (destStats.mtimeMs >= sourceStats.mtimeMs) {
         console.log('No need to copy.');
+        return;
       }
-    })
-    .catch(async (error) => {
+    } catch (error) {
       if (error.code === 'ENOENT') {
-        // Directory does not exist, proceed with copying
         await fs.mkdir(destDir, { recursive: true });
       } else {
         throw error;
       }
-    })
-    .then(async () => {
-      fs.readdir(sourceDir, { withFileTypes: true }).then((items) => {
-        items.forEach((item) => {
-          const sourcePath = path.join(sourceDir, item.name);
-          const destPath = path.join(destDir, item.name);
-          if (item.isDirectory()) {
-            copyDirRecursive(sourcePath, destPath);
-          } else {
-            fs.copyFile(sourcePath, destPath);
-          }
-        });
-      });
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+    }
+
+    const sourceFiles = await fs.readdir(sourceDir, { withFileTypes: true });
+    const destFiles = await fs.readdir(destDir, { withFileTypes: true });
+
+    for (const file of destFiles) {
+      if (!sourceFiles.some((f) => f.name === file.name)) {
+        const destPath = path.join(destDir, file.name);
+        await fs.rm(destPath, { recursive: true, force: true });
+        console.log(`Deleted ${file.name} from destDir.`);
+      }
+    }
+
+    for (const item of sourceFiles) {
+      const sourcePath = path.join(sourceDir, item.name);
+      const destPath = path.join(destDir, item.name);
+      if (item.isDirectory()) {
+        await copyDirRecursive(sourcePath, destPath);
+      } else {
+        await fs.copyFile(sourcePath, destPath);
+      }
+    }
+
+    console.log('Files copied successfully!');
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
-function copyDirRecursive(sourceDir, destDir) {
-  fs.mkdir(destDir, { recursive: true })
-    .then(() => fs.readdir(sourceDir, { withFileTypes: true }))
-    .then((items) => {
-      items.forEach((item) => {
-        const sourcePath = path.join(sourceDir, item.name);
-        const destPath = path.join(destDir, item.name);
-        if (item.isDirectory()) {
-          copyDirRecursive(sourcePath, destPath);
-        } else {
-          fs.copyFile(sourcePath, destPath);
-        }
-      });
-    });
+async function copyDirRecursive(sourceDir, destDir) {
+  await fs.mkdir(destDir, { recursive: true });
+  const items = await fs.readdir(sourceDir, { withFileTypes: true });
+  for (const item of items) {
+    const sourcePath = path.join(sourceDir, item.name);
+    const destPath = path.join(destDir, item.name);
+    if (item.isDirectory()) {
+      await copyDirRecursive(sourcePath, destPath);
+    } else {
+      await fs.copyFile(sourcePath, destPath);
+    }
+  }
 }
 
 copyDir();
